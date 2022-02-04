5f58cb2e2db46913ceb1edba6bc8b433882acd5c
Nick Garza <thenickgarza14@gmail.com>
2022-02-04 13:45:45 -0600

    trying to get the viewDeparments function to work

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,15 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 // Connects to the DATABASE
 const db = mysql.createConnection(
     {
-        host: process.env.HOST,
+        host: 'localhost',
         // My MySQL username
         user: process.env.DB_USER,
         // Your MySQL password
-        password: process.env.DB_PASSWORD,
+        password: process.env.DB_PW,
         database: process.env.DB_NAME,
     },
     console.log('Connected to the Employee Tracker database.')
--- server.js
+++ server.js
@@ -1,3 +1,69 @@
-const express = require('express');
-const mysql2 = require('mysql2');
 const console_log = require('console.table');
+const db = require('./db/connection');
+const inquirer = require('inquirer');
+const { allowedNodeEnvironmentFlags, exit } = require('process');
+
+mainScreen = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'userOptions',
+            message: 'Hello!, Plese choose how you would like to continue',
+            choices: [
+                'View all departments',
+                'View all employees',
+                'View all roles',
+                'Add a department',
+                'Add a employee',
+                'Add a role',
+                'Update employee role',
+                'Delete an employee',
+                'Exit'
+            ]
+        }
+    ]).then(function (answer) {
+        console.log(answer)
+        switch (answer) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a employee':
+                addEmployee();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Update employee role':
+                updateRole();
+                break;
+            case 'Delete an employee':
+                deleteEployee();
+                break;
+            case 'Exit':
+                exit();
+                break;
+        }
+    })
+};
+
+viewDepartments = () => {
+    const sql = 'SELECT * FROM department';
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.log(rows);
+        console.table(rows);
+        mainScreen();
+    })
+};
+
+mainScreen();
